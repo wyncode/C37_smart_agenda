5e7c73d7a21c338f3937dac4ff33e8c1b2c1d257
bootros1 <bootros2002@gmail.com>
2020-06-18 20:21:37 -0400

    initial

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Customer name</h1>
+      <h1>Customer ZIP</h1>
+      <h1>Customer name</h1>
+      <h1>Customer name</h1>
+    </div>
+  );
+};
+
+export default Contacts;
